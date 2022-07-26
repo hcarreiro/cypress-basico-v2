601f4aaeaeb239a538e69cc30b58a6c9d670b444
Hildefonso Carreiro <hildefonso.carreiro@gmail.com>
2022-07-26 00:45:31 -0300

    inicio get

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -3,9 +3,17 @@
 // suíte de teste
 describe('Central de Atendimento ao Cliente TAT', function() {
 
+    beforeEach( function() {
+
+        // para execução antes de cada caso de teste (cada it)
+        it('verifica o título da aplicação', function() {
+            cy.visit('./src/index.html');
+            cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT');
+        })
+    })
+
     // caso de teste
-    it('verifica o título da aplicação', function() {
-        cy.visit('./src/index.html');
-        cy.title().should('eq', 'Central de Atendimento ao Cliente TAT');
+    it('preenche os campos obrigatórios e envia o formulário', function() {
+        
     })
 })
